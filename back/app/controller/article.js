fd8ab03fc5a158e3af3a15ea56995e40afaed0d7
timeLorder <lyh9134wl@gmail.com>
2020-12-05 19:00:59 +0800

    feat: update part of view page and add draft model

--- back/app/controller/article.js
+++ back/app/controller/article.js
@@ -18,15 +18,27 @@ class ArticleController extends BaseController {
 
   // 发布
   async publish() {
-    // const {
-    //   ctx,
-    //   config: { jwtTokenKey },
-    // } = this;
-    // try {
-    //   this.success();
-    // } catch (error) {
-    //   this.error();
-    // }
+    const { ctx } = this;
+    const { title, cover, html, md } = ctx.request.body;
+    const { _id } = ctx.state.user;
+    const { _id: articleId } = await ctx.model.Article.create({
+      title,
+      cover,
+      _article: md,
+      article: html,
+      author: _id,
+    });
+    this.success({ data: { id: articleId, message: '发布成功' } });
+  }
+
+  // 查询详情
+  async detail() {
+    const { ctx } = this;
+    const { id } = ctx.query;
+    const info = await ctx.model.Article.find({ _id: id })
+      .populate('author')
+      .update({ $inc: { readTimes: 1 } });
+    this.success({ data: info });
   }
 }
 
