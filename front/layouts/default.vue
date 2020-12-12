79e4a097ac0dc5d01929c7fb7cd118496be337c7
timeLorder <lyh9134wl@gmail.com>
2020-12-13 02:23:49 +0800

    feat: reform index page

--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
     <wd-header />
-    <Nuxt />
+
+    <main id="wd-content-wrapper">
+      <Nuxt />
+    </main>
+
     <wd-footer />
   </div>
 </template>
+
+<style lang="less">
+#wd-content-wrapper {
+  width: @index-width;
+  margin: 30px auto;
+}
+</style>
